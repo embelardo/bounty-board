b25a0f3bc4a3029acd47083ed3e94a625a3ac22f
Miko Belardo <embelardo@gmail.com>
2022-01-13 19:12:02 -0500

    Add ch6 content

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,21 @@
 const val HERO_NAME = "Madrigal"
-var playerLevel = 5
+var playerLevel = 0
 
 fun main() {
-    println("The hero announces her presence to the world.")
-
-    println(HERO_NAME)
-    println(playerLevel)
+    println("$HERO_NAME announces her presence to the world.")
+    println("What level is $HERO_NAME?")
+    val playerLevelInput = readLine()!!
+    playerLevel = if (playerLevelInput.matches("""\d+""".toRegex())) {
+        playerLevelInput.toInt()
+    } else {
+        1
+    }
+    println("$HERO_NAME's level is $playerLevel")
 
     readBountyBoard()
 
     println("Time passes...")
-    println("The hero returns from her quest.")
+    println("$HERO_NAME returns from her quest.")
 
     playerLevel += 1
     println(playerLevel)
@@ -18,8 +23,13 @@ fun main() {
 }
 
 private fun readBountyBoard() {
-    println("The hero approaches the bounty board. It reads:")
-    println(obtainQuest(playerLevel))
+    println(
+        """
+        |$HERO_NAME approaches the bounty board. It reads:
+        |    "${obtainQuest(playerLevel)
+            .replace("Nogartse", "xxxxxxxx")}"
+        """.trimMargin()
+    )
 }
 
 private fun obtainQuest(
